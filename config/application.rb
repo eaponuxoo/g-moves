0f527a3f07fe15d04c33a62b94d4031fe7585adf
shunyamm <gnjwth@gmail.com>
2017-04-24 13:32:38 +0900

    add new file

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,12 @@ module GMoves
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Tokyo'
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
